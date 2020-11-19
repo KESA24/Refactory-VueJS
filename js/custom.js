01dce22e9d3b40b9951a5f97d0598396ea9de28c
Kesa <nekepatri@gmail.com>
2020-11-19 13:26:38 +0300

    Started on VueJS

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+ instance1 = new Vue({
+    el : '#root',
+      data : { title: 'Catalyst Students', student :'Kesa'},
+
+      methods: {
+        greetKesa: function(){
+          alert('Hi Kesa');
+        }
+      }
+  });
+  
+  

ca8365c065d1f946ce5ffa76975b7eb8c53e9cea
Kesa <nekepatri@gmail.com>
2020-11-21 08:17:46 +0300

    Tv set designed, checkbox not working

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,38 @@
       }
   });
   
-  
+  new Vue({
+    el: "#root",
+// the data property
+    data(){
+      return{
+        signal: false
+      }
+    },
+// the methods property
+    methods:{
+      toggle: function(){
+        this.signal= !this.signal;
+      }
+    }
+  })
+
+  new Vue({
+    el: "#root",
+// the data property
+    data(){
+      return{
+        signal: false,
+        items:["item1", "item2", "item3", "item4", "item5"],
+
+        //imagespath
+        imageSource: "../images/images2jpeg"
+      }
+    },
+// the methods property
+    methods:{
+      toggle: function(){
+        this.signal= !this.signal;
+      }
+    }
+  })
